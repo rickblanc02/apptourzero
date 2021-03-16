a5ef5710ac3fa960d7305b4c0d824b1c445b26d0
rickblanc02 <rickblanc02@gmail.com>
2021-03-16 15:58:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/venezuelabackpackers'));
-app.get('/*', function(req,res) {
+
+//app.use(express.static(__dirname + '/venezuelabackpackers/dist/'));
+app.use(express.static(__dirname, 'dist', {index: false}));
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+/*app.get('*', (req, res) => {
+    res.sendFile(`./venezuelabackpackers/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});*/
+
+/*app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/venezuelabackpackers/index.html'));});
+*/
+
 app.listen(process.env.PORT || 8080);
+
