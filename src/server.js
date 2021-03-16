add023ddce8f5c14831262dc036538a875841db5
rickblanc02 <rickblanc02@gmail.com>
2021-03-16 14:53:38 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,17 +8,23 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 
-//const path = require('path');
+const path = require('path');
 
 app.use(requireHTTPS);
 
 app.use(express.static('./dist/venezuelabackpackers'));
 
-app.get('/*', (req, res) =>
+/*app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/venezuelabackpackers/src/'}),
-);
+);*/
 /*app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/venezuelabackpackers/index.html'));    
 });*/
+
+app.get('/*', function(req,res){
+    const fullPath = path.join(__dirname + '/dist/venezuelabackpackers/index.html');
+    console.log(" Fetching from.." + fullPath);
+      res.sendFile(fullPath);
+  })
 
 app.listen(process.env.PORT || 8080);
