89a26037b568640d7ffdffc5f3c021e09c368780
rickblanc02 <rickblanc02@gmail.com>
2021-03-16 15:19:02 -0400

    u

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
+
 const app = express();
-
-const path = require('path');
-
-app.use(requireHTTPS);
 
 app.use(express.static('./dist/venezuelabackpackers'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/venezuelabackpackers/'}),
-);
-/*app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/venezuelabackpackers/index.html'));    
-});*/
-
-/*app.get('/*', function(req,res){
-    const fullPath = path.join(__dirname + '/dist/venezuelabackpackers/index.html');
-    console.log(" Fetching from.." + fullPath);
-      res.sendFile(fullPath);
-  })*/
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/venezuelabackpackers' }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
