570f625037f336b7ba57d99545b38c40b1c9a683
rickblanc02 <rickblanc02@gmail.com>
2021-03-16 16:12:09 -0400

    u

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
+//Install express server
 const express = require('express');
+// Requirement of path Module in order to manage URLS
 const path = require('path');
 const app = express();
-
-//app.use(express.static(__dirname + '/venezuelabackpackers/dist/'));
-app.use(express.static(__dirname, 'dist', {index: false}));
-
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/venezuelabackpackers'));
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname+'/dist/venezuelabackpackers/index.html'));
 });
-
-/*app.get('*', (req, res) => {
-    res.sendFile(`./venezuelabackpackers/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});*/
-
-/*app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/venezuelabackpackers/index.html'));});
-*/
-
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
